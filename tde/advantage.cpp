422c9a355bc7896acde84f91ad603dcd238f24a5
Vi1i Petal <vi1i.petal@gmail.com>
2019-02-05 17:15:03 -0500

    Converted over to getters with a struct for the contructor param

--- tde/advantage.cpp
+++ tde/advantage.cpp
@@ -6,17 +6,42 @@
 
 #include <string>
 
-tde::advantage::advantage(const std::string &name,
-                          const std::string &description,
-                          const std::string &rules,
-                          const std::string &range,
-                          const std::string &actions,
-                          const std::string &prerequisites,
-                          int ap_value) :
-                          name_(name),
-                          description_(description),
-                          rules_(rules),
-                          range_(range),
-                          actions_(actions),
-                          prerequisites_(prerequisites),
-                          ap_value_(ap_value) {}
+
+axel::tde::advantage::advantage(struct axel::tde::advantage::data datum) :
+        name_(datum.name),
+        description_(datum.description),
+        rules_(datum.rules),
+        range_(datum.range),
+        actions_(datum.actions),
+        prerequisites_(datum.prerequisites),
+        ap_value_(datum.ap_value) {}
+
+axel::tde::advantage::~advantage() = default;
+
+auto axel::tde::advantage::getName() -> std::string {
+    return name_;
+}
+
+auto axel::tde::advantage::getDescription() -> std::string {
+    return description_;
+}
+
+auto axel::tde::advantage::getRules() -> std::string {
+    return rules_;
+}
+
+auto axel::tde::advantage::getRange() -> std::string {
+    return range_;
+}
+
+auto axel::tde::advantage::getActions() -> std::string {
+    return actions_;
+}
+
+auto axel::tde::advantage::getPrerequisites() -> std::string {
+    return prerequisites_;
+}
+
+auto axel::tde::advantage::getApValue() -> int {
+    return ap_value_;
+}
